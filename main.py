acdc6546b2fe4572e0e54a98197e8b1180541418
dafas <jc742323@gmail.com>
2022-11-22 18:32:52 +0900

    Ass2 수정

--- main.py
+++ main.py
@@ -74,7 +74,9 @@ def getHPWL(net_list, cell_list, pad_list):
 
 def solve_AX_b(A,b):
     inverse_A = np.linalg.inv(A)
+    #print(inverse_A)
     result = np.dot(b,inverse_A)
+    #print(b)
     return result
 
 def QuadraticPlacement(net_list, cell_list, pad_list):
@@ -85,7 +87,7 @@ def QuadraticPlacement(net_list, cell_list, pad_list):
     weight = [[0 for _ in range(cell_number+pad_number)] for _ in range(cell_number+pad_number)]
 
     #################################
-    # net로 서로 연결된 pad와 cell을 조사하여 가중치 표를 만듦.
+    # STEP1: net로 서로 연결된 pad와 cell을 조사하여 가중치 표를 만듦.
 
     for i in range(len(net_list)):
         net_i: data_structures.Net = net_list[i]
@@ -97,26 +99,27 @@ def QuadraticPlacement(net_list, cell_list, pad_list):
             theCell: data_structures.Cell = cell_list[net_i.connected_cells[j] - 1]
             cell_id = int(theCell.id)
             cell_id_and_pad_id_for_net_i.append(cell_id)
+
         for j in range(connected_pad_num):
             thePad: data_structures.Pad = pad_list[net_i.connected_pad[j] - 1]
             pad_id = int(thePad.pad_id)
             cell_id_and_pad_id_for_net_i.append(pad_id)
-
-        for j in range(connected_cell_num+connected_pad_num):
-            for k in range(1, connected_cell_num+connected_pad_num - j-1):
+        for j in range(len(cell_id_and_pad_id_for_net_i)):
+            for k in range(1, len(cell_id_and_pad_id_for_net_i) - j):
                 if j<connected_cell_num and j+k<connected_cell_num:
                     weight[cell_id_and_pad_id_for_net_i[j]][cell_id_and_pad_id_for_net_i[j+k]] += 1  # 한 net로 연결되어 있는 Cell들을 콕콕 집어가며 표에 1을 더해줌.
                     weight[cell_id_and_pad_id_for_net_i[j+k]][cell_id_and_pad_id_for_net_i[j]] += 1
                 elif j<connected_cell_num and j+k>=connected_cell_num:
-                    weight[cell_id_and_pad_id_for_net_i[j]][connected_cell_num+cell_id_and_pad_id_for_net_i[j+k]] += 1  # 한 net로 연결되어 있는 Cell들을 콕콕 집어가며 표에 1을 더해줌.
-                    weight[connected_cell_num+cell_id_and_pad_id_for_net_i[j+k]][cell_id_and_pad_id_for_net_i[j]] += 1
-                elif j >= connected_cell_num and connected_cell_num:
-                    weight[connected_cell_num+cell_id_and_pad_id_for_net_i[j]][connected_cell_num + cell_id_and_pad_id_for_net_i[j + k]] += 1  # 한 net로 연결되어 있는 Cell들을 콕콕 집어가며 표에 1을 더해줌.
-                    weight[connected_cell_num + cell_id_and_pad_id_for_net_i[j + k]][connected_cell_num+cell_id_and_pad_id_for_net_i[j]] += 1
+                    weight[cell_id_and_pad_id_for_net_i[j]][cell_number+cell_id_and_pad_id_for_net_i[j+k]] += 1  # 한 net로 연결되어 있는 Cell들을 콕콕 집어가며 표에 1을 더해줌.
+                    weight[cell_number+cell_id_and_pad_id_for_net_i[j+k]][cell_id_and_pad_id_for_net_i[j]] += 1
+                elif j >= connected_cell_num and j+k>connected_cell_num:
+                    weight[cell_number+cell_id_and_pad_id_for_net_i[j]][cell_number + cell_id_and_pad_id_for_net_i[j + k]] += 1  # 한 net로 연결되어 있는 Cell들을 콕콕 집어가며 표에 1을 더해줌.
+                    weight[cell_number + cell_id_and_pad_id_for_net_i[j + k]][cell_number+cell_id_and_pad_id_for_net_i[j]] += 1
 
     # delete the completely used variables
     del connected_cell_num, theCell, i, j, k
 
+    # STEP2: 미분을 했을 때 0되게 하는 X를 구하기 위해 행렬 식 만듦
     diagonal = [[0 for _ in range(cell_number)] for _ in range(cell_number)]
     for i in range(cell_number):
         sum = 0
@@ -130,55 +133,63 @@ def QuadraticPlacement(net_list, cell_list, pad_list):
             Connectivitiy_matrix[i][j] += weight[i][j]
 
     A = np.matrix(diagonal) - np.matrix(Connectivitiy_matrix)
-    for i in range(cell_number):
-        print(diagonal[i])
+
     # for x,
     b_x = [[0 for _ in range(1)] for _ in range(cell_number)]
 
-    for k in range(cell_number):
-        for i in range(pad_number):
-            for j in range(cell_number):
-                if weight[i+cell_number][j] != 0:
-                    b[0][k] += weight[i+cell_number][j]*pad_list[i].coordinate[0]
-                    break
+    k = 0
+    for i in range(pad_number):
+        for j in range(cell_number):
+            if weight[i+cell_number][j] != 0:
+                b_x[k][0] += weight[i+cell_number][j]*pad_list[i].coordinate[0]
+                k += 1
+                break
+
     # for y,
     b_y = [[0 for _ in range(1)] for _ in range(cell_number)]
 
-    for k in range(cell_number):
-        for i in range(pad_number):
-            for j in range(cell_number):
-                if weight[i + cell_number][j] != 0:
-                    b[0][k] += weight[i + cell_number][j] * pad_list[i].coordinate[1]
-                    break
+    k = 0
+    for i in range(pad_number):
+        for j in range(cell_number):
+            if weight[i + cell_number][j] != 0:
+                b_y[k][0] += weight[i + cell_number][j] * pad_list[i].coordinate[1]
+                k += 1
+                break
 
+    b_x = np.matrix(b_x)
+    b_y = np.matrix(b_y)
+
+    b_x = np.transpose(b_x)
+    b_y = np.transpose(b_y)
+
+    # STEP3: 최적의 (x,y)를 구함.
     cells_x = solve_AX_b(A,b_x) # Quadratic Placement의 cell의 x좌표들(세로로 배열되어 있음)
     cells_y = solve_AX_b(A,b_y) # Quadratic Placement의 cell의 y좌표들(세로로 배열되어 있음)
 
-    # 위에서 구한 x,y를 바탕으로 cell의 좌표를 바꿔줄것임.
+    # STEP4: 위에서 구한 x,y를 바탕으로 cell의 좌표를 바꿔줄것임.
     for i in range(len(cell_list)):
-
         temp = int(cell_list[i].coordinate[0])
         cell_list[i].coordinate[0] -= temp
-        cell_list[i].coordinate[0] += cells_x[i]
+        cell_list[i].coordinate[0] += cells_x[0,i]
 
-        temp = int(theCell.coordinate[1])
+        temp = int(cell_list[i].coordinate[1])
         cell_list[i].coordinate[1] -= temp
-        cell_list[i].coordinate[1] += cells_y[i]
+        cell_list[i].coordinate[1] += cells_y[0,i]
 
-    del i, j, k, theCell
 
 if __name__ == '__main__':
-    filename = "benchmarks/toy2"
-    net_list, cell_list, pad_list = parsing(filename)
+    #for filename in ["benchmarks/biomed","benchmarks/industry1","benchmarks/industry2", "benchmarks/primary","benchmarks/struct","benchmarks/toy1", "benchmarks/toy2"]:
+        filename = "benchmarks/toy1"
+        net_list, cell_list, pad_list = parsing(filename)
 
-    # random placement
-    draw_window(pad_list, cell_list, net_list, ver="visualization")
-    HPWL = getHPWL(net_list, cell_list, pad_list)
-    print("Random Placement HPWL=", HPWL)
+        # random placement
+        draw_window(pad_list, cell_list, net_list, ver="visualization")
+        HPWL = getHPWL(net_list, cell_list, pad_list)
+        print("Random Placement HPWL=", HPWL)
 
-    # quadraticPlacement
-    QuadraticPlacement(net_list, cell_list, pad_list)
-    draw_window(pad_list, cell_list, net_list, ver="visualization")
-    HPWL = getHPWL(net_list, cell_list, pad_list)
-    print("Quadratic Placement HPWL=", HPWL)
+        # quadraticPlacement
+        QuadraticPlacement(net_list, cell_list, pad_list)
+        draw_window(pad_list, cell_list, net_list, ver="visualization")
+        HPWL = getHPWL(net_list, cell_list, pad_list)
+        print("Quadratic Placement HPWL=", HPWL)
 
